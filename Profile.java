8719ca8a716e28d8db8d05d777141b92bd966364
Alejandro Diaz Meinecke <130598365+admc1337@users.noreply.github.com>
2023-05-03 17:17:33 -0400

    Add files via upload

---
+++ Profile.java
@@ -0,0 +1,40 @@
+package com.example.is413project;
+
+public class Profile {
+    //profile data
+    String name, phone, email;
+    //validation for bookData entry
+    Boolean entered = false;
+    Profile(String n, String p, String e){
+        name = n;
+        phone = p;
+        email = e;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public Boolean getEntered(){
+        return entered;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setEntered(Boolean entered) {
+        this.entered = entered;
+    }
+}
